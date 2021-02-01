885f4baebc5034b1d6a5f9f3af2c1826a8948a23
Domenico Daniel Giovinazzo <domenicodanielgiovinazzo@gmail.com>
2021-01-31 20:53:11 -0500

    Complete backend user auth

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const User = require('./models/User')
+
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
+
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+    const user = new User({
+        handle: 'Daniel',
+        email: 'test@test.com',
+        password: '12345'
+    })
+    user.save()
+    res.send('Hello World')
+})
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
