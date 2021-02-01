7f64e8460bfb077f9d1dc74f3ec5a59de064ada6
gscarborough <gigimscarborough@gmail.com>
2021-02-01 14:09:56 -0500

    create main modal

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,14 +1,23 @@
 
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
+import {openModal} from '../../actions/modal_actions'
 
 import NavBar from './navbar';
+import modal from '../session/modal';
 
-const mapStateToProps = state => ({
+const mSTP = state => ({
     loggedIn: state.session.isAuthenticated
 });
 
+const mDTP = dispatch => ({
+    logout : () => dispatch(logout()),
+    openModal : modal => dispatch(openModal(modal))
+});
+
+
+
 export default connect(
-    mapStateToProps,
-    { logout }
+    mSTP,
+    mDTP
 )(NavBar);
---
+++ frontend/src/reducers/ui_reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+import modalReducer from './modal_reducer'
+
+
+
+const uiReducer = combineReducers({
+    modal: modalReducer
+
+})
+
+
+export default uiReducer
