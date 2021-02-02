a097ecd063da12036b0dd382d262ee41d3efc222
Hojung Cha <hoj0210@gmail.com>
2021-02-02 12:28:24 -0800

    working on the show page, committing to pull

--- frontend/src/components/trainer/trainer_show.jsx
+++ frontend/src/components/trainer/trainer_show.jsx
@@ -8,13 +8,34 @@ class TrainerShow extends React.Component{
     }
 
     componentDidMount(){
-
+        this.props.getTrainer(this.props.match.params.trainerId)
     }
 
     render(){
         return(
             <div>
+                <div className="trainer-intro-container">
+                    <div className="trainer-profile-box">
+                        <img src={this.props.trainer.imageUrl} />
+                        <div className="trainer-profile">
+                            <p>{this.props.trainer.firstName} {this.props.trainer.lastName}</p>
+                            <p>THIS IS WHERE THE AVG RATING GOES??</p>
+                            <div className="trainer-specialties">
+                                {this.props.trainer.specialties.map((specialty, i) => (
+                                    <ul key={i} className="trainer-specialty">{specialty}</ul>
+                                ))}
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="">
 
+                </div>
+                <div>
+
+                </div>
             </div>
         )
     }
--- frontend/src/components/trainer/trainer_show_container.js
+++ frontend/src/components/trainer/trainer_show_container.js
@@ -1,15 +1,16 @@
 import TrainerShow from './trainer_show';
 import { connect } from 'react-redux';
+import { getTrainer } from '../../actions/trainer_actions'
 
 const msp = (state, ownProps) => (
     {
-        
+        trainer: state.trainers[ownProps.match.params.trainerId]
     }
 )
 
 const mdp = dispatch => (
     {
-
+        getTrainer: (trainerId) => dispatch(getTrainer(trainerId))
     }
 )
 
