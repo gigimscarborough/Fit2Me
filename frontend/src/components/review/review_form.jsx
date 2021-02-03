1b8c7f19ac5a247b0e3a39c41fd8f9550ae98ede
Hojung Cha <hoj0210@gmail.com>
2021-02-03 15:52:08 -0800

    working on review-form

--- frontend/src/components/review/review_form.jsx
+++ frontend/src/components/review/review_form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom'
+import './review_form.scss'
 
 class ReviewForm extends React.Component {
     constructor(props){
@@ -27,7 +28,7 @@ class ReviewForm extends React.Component {
 
     handleSubmit(e){
         e.preventDefault(); 
-        this.props.createReview(this.state).then(()=> this.props.history.push(`/trainers/${this.state.trainerId}`))
+        this.props.createReview(this.state).then(()=> this.props.history.push(`/trainers/${this.props.trainer.id}`))
     }
 
     render(){
@@ -38,7 +39,7 @@ class ReviewForm extends React.Component {
             )
         } else {
             return(
-                <div>
+                <div className="review-container">
                     <div className="review-form-main">
                         <form onSubmit={this.handleSubmit}>
                             <div className="review-form-box">
@@ -46,8 +47,10 @@ class ReviewForm extends React.Component {
                                     <span className="review-form-trainer-name">{this.props.trainer.name}</span>
                                 </div>
                                 <div className="review-content-box">
-                                    <p className="select-rating-text">Select your rating</p>
-                                    <input type="number" min="1" max="5" className="rating-number"value={this.state.rating} onChange={this.handleChange("rating")}/>
+                                    <div className="">
+                                        <p className="select-rating-text">Select your rating</p>
+                                        <input type="number" min="1" max="5" className="rating-number"value={this.state.rating} onChange={this.handleChange("rating")}/>
+                                    </div>                                   
                                     <textarea className="text-area-text"value={this.state.body} onChange={this.handleChange("body")} 
                                     placeholder="If it weren't for Jim, I don't know how I would have kept my body this way during the Covid! He is so fantastic with the equipment, and his workout routine helped me get in shape even during this time!!"></textarea>
                                 </div>
