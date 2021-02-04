0e78c42b787de9aa1d315564b4e44aba3172e153
ralles <ralles.liu@gmail.com>
2021-02-04 09:18:07 -0800

    fixed bugs in location

--- frontend/src/reducers/users_api_reducer.js
+++ frontend/src/reducers/users_api_reducer.js
@@ -4,6 +4,7 @@ export default function (state = {}, action) {
     Object.freeze(state)
     const newState = Object.assign({}, state)
     switch (action.type) {
+        
         case RECEIVE_USER:
             newState[action.user.data['_id']] = action.user.data
             return newState
