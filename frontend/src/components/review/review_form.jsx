b69c75d03294824274f6664414cf50ffa0da45b2
Hojung Cha <hoj0210@gmail.com>
2021-02-03 21:33:35 -0800

    styled the review form page

--- frontend/src/components/review/review_form.jsx
+++ frontend/src/components/review/review_form.jsx
@@ -24,15 +24,16 @@ class ReviewForm extends React.Component {
     componentDidMount(){
         
         this.props.getTrainer(this.props.match.params.trainerId)
-        if (!this.props.user_id) {
-            this.props.openModal("login").then()
-            return(
-                <Redirect to="/"/>
-            )
-        }
+        // if (!this.props.user_id) {
+        //     this.props.openModal("login").then()
+        //     return(
+        //         <Redirect to="/"/>
+        //     )
+        // }
     }
 
     handleSubmit(e){
+        debugger
         e.preventDefault(); 
         this.props.createReview(this.state).then(()=> this.props.history.push(`/trainers/${this.props.trainer.id}`))
     }
@@ -55,9 +56,29 @@ class ReviewForm extends React.Component {
                                 </div>
                                 <div className="review-content-box">
                                     <div className="review-content">
-                                        <p className="select-rating-text">Select your rating</p>
-                                        <input type="number" min="1" max="5" className="rating-number"value={this.state.rating} onChange={this.handleChange("rating")}/>
-                                    </div>                                   
+                                        <p className="select-rating-text">How would you rate your workout with him?</p>
+                                        <span className="rating-radio-button">
+                                            <input className="radio-button" type="radio" name="trainer-rating" id="1" value={this.state.rating} onChange={this.handleChange('rating')}/>
+                                            <label htmlFor="1">{1}</label>
+                                        </span>
+                                        <span className="rating-radio-button">
+                                            <input className="radio-button"type="radio" name="trainer-rating" id="2" value={this.state.rating} onChange={this.handleChange('rating')} />
+                                            <label htmlFor="2">{2}</label>
+                                        </span>
+                                        <span className="rating-radio-button">
+                                            <input className="radio-button"type="radio" name="trainer-rating" id="3" value={this.state.rating} onChange={this.handleChange('rating')}/>
+                                            <label htmlFor="3">{3}</label>
+                                        </span>
+                                        <span className="rating-radio-button">
+                                            <input className="radio-button"type="radio" name="trainer-rating" id="4" value={this.state.rating} onChange={this.handleChange('rating')}/>
+                                            <label htmlFor="4">{4}</label>
+                                        </span>
+                                        <span className="rating-radio-button">
+                                            <input className="radio-button"type="radio" name="trainer-rating" id="5" value={this.state.rating} onChange={this.handleChange('rating')}/>
+                                            <label htmlFor="5">{5}</label>
+                                        </span>
+                                    </div>
+                                    <h3 className="review-body">WRITE YOUR REVIEW</h3>                                   
                                     <textarea className="text-area-text"value={this.state.body} onChange={this.handleChange("body")} 
                                     placeholder="If it weren't for Jim, I don't know how I would have kept my body this way during the Covid! He is so fantastic with the equipment, and his workout routine helped me get in shape even during this time!!"></textarea>
                                 </div>
